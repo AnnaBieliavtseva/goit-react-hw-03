85c0047b1bf0087ee29cb16c017e8055126e7e20
Anna Bieliavtseva <belyavtseva1911@gmail.com>
2025-01-12 12:12:14 +0100

    add exports/imports of components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,20 @@
 // import { useState } from 'react'
 import './App.css'
+import ContactForm from './ContactForm/ContactForm';
+import ContactList from './ContactList/ContactList';
+import SearchBox from './SearchBox/SearchBox';
 
 function App() {
   
 
   return (
     <>
-     
-      <h1>Vite + React</h1>
-      
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <SearchBox/>
+      <ContactList />
     </>
-  )
+  );
 }
 
 export default App
